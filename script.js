33105d6efb7da774e3e9c980fa838a670c1fd5a0
shubham-1903 <shubhamkr7677@gmail.com>
2021-10-03 12:25:21 +0530

    added icons and feature backgroung change on click

--- script.js
+++ script.js
@@ -5,70 +5,91 @@ var count = 0;
 
 var timer = false;
 
+let colorStart = false;
+let colorStop = false;
+let colorReset = false;
+
 function start() {
-    timer = true;
-    stopwatch();
+	document.body.style.setProperty(
+		"background",
+		"linear-gradient(to left, #3494e6, #ec6ead)",
+		"important"
+	);
+
+	timer = true;
+	stopwatch();
 }
 
 function stop() {
-    timer = false;
-
+	document.body.style.setProperty(
+		"background",
+		"linear-gradient(to left, #240b36, #c31432)",
+		"important"
+	);
+	timer = false;
 }
 
 function reset() {
-    timer = false;
+	document.body.style.setProperty(
+		"background",
+		"linear-gradient(to right, #191654, #43C6AC)",
+		"important"
+	);
+	timer = false;
 
-    hr = 0;
-    min = 0;
-    sec = 0;
-    count = 0;
+	hr = 0;
+	min = 0;
+	sec = 0;
+	count = 0;
 
-    document.getElementById("hr").innerHTML = "00";
-    document.getElementById("min").innerHTML = "00";
-    document.getElementById("sec").innerHTML = "00";
-    document.getElementById("count").innerHTML = "00";
-
+	document.getElementById("hr").innerHTML = "00";
+	document.getElementById("min").innerHTML = "00";
+	document.getElementById("sec").innerHTML = "00";
+	document.getElementById("count").innerHTML = "00";
 }
 
 function stopwatch() {
-    if (timer == true)
-        count = count + 1;
+	if (timer == true) count = count + 1;
 
-    if (count == 99) {
-        sec = sec + 1;
-        count = 0;
-    }
-    if (sec == 59) {
-        min = min + 1;
-        sec = 0;
-    }
-    if (min == 59) {
-        hr = hr + 1;
-        min = 0;
-        sec = 0;
-    }
+	if (count == 99) {
+		sec = sec + 1;
+		count = 0;
+	}
+	if (sec == 59) {
+		min = min + 1;
+		sec = 0;
+	}
+	if (min == 59) {
+		hr = hr + 1;
+		min = 0;
+		sec = 0;
+	}
 
-    var hrString = hr;
-    var minString = min;
-    var secString = sec;
-    var countString = count;
+	var hrString = hr;
+	var minString = min;
+	var secString = sec;
+	var countString = count;
 
-    if (hr < 10) {
-        hrString = "0" + hrString;
-    }
-    if (min < 10) {
-        minString = "0" + minString;
-    }
-    if (sec < 10) {
-        secString = "0" + secString;
-    }
-    if (count < 10) {
-        countString = "0" + countString;
-    }
+	if (hr < 10) {
+		hrString = "0" + hrString;
+	}
+	if (min < 10) {
+		minString = "0" + minString;
+	}
+	if (sec < 10) {
+		secString = "0" + secString;
+	}
+	if (count < 10) {
+		countString = "0" + countString;
+	}
 
-    document.getElementById("hr").innerHTML = hrString;
-    document.getElementById("min").innerHTML = minString;
-    document.getElementById("sec").innerHTML = secString;
-    document.getElementById("count").innerHTML = countString;
-    setTimeout("stopwatch()", 10)
+	document.getElementById("hr").innerHTML = hrString;
+	document.getElementById("min").innerHTML = minString;
+	document.getElementById("sec").innerHTML = secString;
+	document.getElementById("count").innerHTML = countString;
+	setTimeout("stopwatch()", 10);
 }
+
+/*background: #c31432;  /* fallback for old browsers */
+// background: -webkit-linear-gradient(to left, #240b36, #c31432);  /* Chrome 10-25, Safari 5.1-6 */
+// background: linear-gradient(to left, #240b36, #c31432); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
