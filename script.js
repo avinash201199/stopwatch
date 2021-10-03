1bf0853007524bb558506fd5f5b003c44cf1edc6
parzival272000 <mayankjha272000@gmail.com>
2021-10-03 13:03:06 +0530

    made start a toggle button

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ var hr = 0;
 var min = 0;
 var sec = 0;
 var count = 0;
-
+var timerprev=false;
 var timer = false;
 
 function start() {
-    timer = true;
+    if(timerprev==false)
+    {
+        timer = true;
+        timerprev=timer;
+    }
+    else
+    {
+        timer=false;
+        timerprev=timer;
+    }
     stopwatch();
 }
 
