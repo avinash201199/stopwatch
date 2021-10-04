f1b981a5cdea4886ff8d8cea3e54e9c8afb9b2a8
Jakub Topoliński <topolinskijakub5@gmail.com>
2021-10-04 19:51:49 +0200

    Move theme change code to separate functions

--- jquery.js
+++ jquery.js
@@ -1,35 +1,38 @@
+function setLightTheme() {
+    $('.navbar.navbar-expand-lg.navbar-light').addClass('bg-light');
+    $('#title1').css({ "color": "black" });
+    $('body').css({ "background": "linear-gradient(to right, #191654, #43C6AC)" })
+    $('.digit').css({ "color": "white" })
+    $('#light').prop("checked", false);
+}
+
+function setDarkTheme() {
+    $('.navbar.navbar-expand-lg.navbar-light').removeClass('bg-light');
+    $('.navbar.navbar-expand-lg.navbar-light').css({ "background-color": "black" });
+    $('#title1').css({ "color": "white" });
+    $('body').css({ "background": "#191212" });
+    $('.digit').css({ "color": "rgb(216 137 31)" });
+    $('#light').prop("checked", true);
+}
+
 $(document).ready(function ()
 {
 
     if (localStorage.getItem("darkmode") == "true")
     {
-        $('.navbar.navbar-expand-lg.navbar-light').removeClass('bg-light');
-        $('.navbar.navbar-expand-lg.navbar-light').css({ "background-color": "black" });
-        $('#title1').css({ "color": "white" });
-        $('body').css({ "background": "#191212" })
-        $('.digit').css({ "color": "rgb(216 137 31)" })
-        $('#light').click();
+        setDarkTheme();
     }
 
     $('#light').on("change paste keyup", function (e)
     {
         if (!e.target.checked)
         {
-            $('.navbar.navbar-expand-lg.navbar-light').addClass('bg-light');
-
-            $('#title1').css({ "color": "black" });
-            $('body').css({ "background": "linear-gradient(to right, #191654, #43C6AC)" })
-            $('.digit').css({ "color": "white" })
-
+            setLightTheme();
             localStorage.setItem("darkmode", false);
         }
         else
         {
-            $('.navbar.navbar-expand-lg.navbar-light').removeClass('bg-light');
-            $('.navbar.navbar-expand-lg.navbar-light').css({ "background-color": "black" });
-            $('#title1').css({ "color": "white" });
-            $('body').css({ "background": "#191212" })
-            $('.digit').css({ "color": "rgb(216 137 31)" })
+            setDarkTheme();
             localStorage.setItem("darkmode", true);
         }
     });
