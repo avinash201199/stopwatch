72ae2a107912405e2650e6d35e5c2b80a4f83bfc
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-10-04 21:20:38 +0530

    changed darkmode button

--- jquery.js
+++ jquery.js
@@ -1,39 +1,36 @@
-$( document ).ready(function() {
+$(document).ready(function ()
+{
 
-    if(localStorage.getItem("darkmode") == "true") {
+    if (localStorage.getItem("darkmode") == "true")
+    {
         $('.navbar.navbar-expand-lg.navbar-light').removeClass('bg-light');
-        $('.navbar.navbar-expand-lg.navbar-light').css({"background-color":"black"});
-        $('#title1').css({"color":"white"});
-        $('#light').hide();
-        $('body').css({"background":"#191212"})
-        $('.digit').css({"color":"rgb(216 137 31)"})
-        $('#dark').show();
-    }else{
-        $('#dark').hide();
+        $('.navbar.navbar-expand-lg.navbar-light').css({ "background-color": "black" });
+        $('#title1').css({ "color": "white" });
+        $('body').css({ "background": "#191212" })
+        $('.digit').css({ "color": "rgb(216 137 31)" })
+        $('#light').click();
     }
-    
-    $('#light').click(function(){
-        $('.navbar.navbar-expand-lg.navbar-light').removeClass('bg-light');
-        $('.navbar.navbar-expand-lg.navbar-light').css({"background-color":"black"});
-        $('#title1').css({"color":"white"});
-        $('#light').hide();
-        $('body').css({"background":"#191212"})
-        $('.digit').css({"color":"rgb(216 137 31)"})
-        $('#dark').show();
 
-        localStorage.setItem("darkmode", true);
+    $('#light').on("change paste keyup", function (e)
+    {
+        if (!e.target.checked)
+        {
+            $('.navbar.navbar-expand-lg.navbar-light').addClass('bg-light');
+
+            $('#title1').css({ "color": "black" });
+            $('body').css({ "background": "linear-gradient(to right, #191654, #43C6AC)" })
+            $('.digit').css({ "color": "white" })
+
+            localStorage.setItem("darkmode", false);
+        }
+        else
+        {
+            $('.navbar.navbar-expand-lg.navbar-light').removeClass('bg-light');
+            $('.navbar.navbar-expand-lg.navbar-light').css({ "background-color": "black" });
+            $('#title1').css({ "color": "white" });
+            $('body').css({ "background": "#191212" })
+            $('.digit').css({ "color": "rgb(216 137 31)" })
+            localStorage.setItem("darkmode", true);
+        }
     });
-
-    $('#dark').click(function(){
-        $('.navbar.navbar-expand-lg.navbar-light').addClass('bg-light');
-        
-        $('#title1').css({"color":"black"});
-        $('#light').show();
-        $('body').css({ "background": "linear-gradient(to right, #191654, #43C6AC)"})
-        $('.digit').css({"color":"white"})
-        $('#dark').hide();
-
-        localStorage.setItem("darkmode", false);
-    });
-
 });
