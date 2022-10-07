5c1f0602f98687bce6bfb93fb2db11c20d8e4d99
Arnab Mandal <arnabmandal90007@gmail.com>
2022-10-07 14:49:22 +0530

    added dark mode in pomodoroTimer, removed scrollbar from pomodoroTimer, added favicon in pomodoroTimer

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -3,6 +3,7 @@ var paused = true
 var minutes
 var timerDate
 var remainingTime = 0
+var darkTheme = false
 
 const audio = new Audio();
 audio.src = "../audio/sound_trim.mp3"  // same audio clip is used for pause,reset
@@ -25,6 +26,7 @@ const setPomoTime = (minutes) => {
     $id('counter-background').classList.remove('inactive');
     $id('counter-background').classList.add('active');
 }
+
 const reset=()=>{
     audio.play();
     if(pomoTime.minutes == 25){
@@ -47,9 +49,21 @@ const startPomoCounter = (action) => {
         $id('counter-background').classList.remove('active');
         $id('counter-background').classList.add('inactive');
         $id('focus').classList.remove('hidden');
+        if(darkTheme){
+            $('.inactive').css({"background": "black", "color": "white"})
+        }
+        else{
+            $('.inactive').css({"background": "rgb(5, 30, 54)", "color": "rgb(169, 188, 214)"});
+        }
     } else {
         $id('counter-background').classList.remove('inactive');
         $id('counter-background').classList.add('active');
+        if(darkTheme){
+            $('.active').css({"color":"#7fe9d4", "background": "#191212"})
+        }
+        else{
+            $('.active').css({ "background": "linear-gradient(to right, #191654, #43C6AC)" });
+        }
     }
     const updateTimer = () => {
         if (!paused) {
@@ -65,3 +79,56 @@ const startPomoCounter = (action) => {
 }
 
 setPomoTime(25)
+
+
+function setLightTheme(){
+    darkTheme = false;
+    $('.navbar').css({ "background-color": "rgb(5, 30, 54)" });
+    $('.active').css({ "background": "linear-gradient(to right, #191654, #43C6AC)" });
+    $('.inactive').css({"background": "rgb(5, 30, 54)"});
+    $('#light').prop("checked", false);
+}
+
+function setDarkTheme(){
+    darkTheme = true;
+    $('.navbar').css({ "background-color": "black" });
+    $('.active').css({"color":"#7fe9d4", "background": "#191212"})
+    $('.inactive').css({"background": "black", "color": "white"})
+    $('#light').prop("checked", true);
+}
+
+var prefersDarkThemeMql = window.matchMedia("(prefers-color-scheme: dark)");
+
+prefersDarkThemeMql.addEventListener("change", function(mql) {
+    if (localStorage.getItem("darkmode") === null && mql.matches) {
+        setDarkTheme();
+    } else {
+        setLightTheme();
+    }
+})
+
+$(document).ready(function ()
+{
+
+    if (
+        localStorage.getItem("darkmode") == "true" ||
+        (localStorage.getItem("darkmode") === null && prefersDarkThemeMql.matches)
+    )
+    {
+        setDarkTheme();
+    }
+
+    $('#light').on("change paste keyup", function (e)
+    {
+        if (!e.target.checked)
+        {
+            setLightTheme();
+            localStorage.setItem("darkmode", false);
+        }
+        else
+        {
+            setDarkTheme();
+            localStorage.setItem("darkmode", true);
+        }
+    });
+});
