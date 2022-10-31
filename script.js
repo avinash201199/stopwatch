a8209530aa0d6c8990deb1167ff0abcc3949d84f
sardarshubham <sardarshubham17@gmail.com>
2022-10-31 18:02:33 +0530

    added new column of absolute differnence between laps

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ var min = 0;
 var sec = 0;
 var count = 0;
 
+var prev_hr = 0;
+var prev_min = 0;
+var prev_sec = 0;
+var prev_count = 0;
+
+var diff_hr = 0;
+var diff_min = 0;
+var diff_sec = 0;
+var diff_count = 0;
+
 
 var timer = false;
 var lapCounter = 1;
@@ -102,12 +112,42 @@ function stopwatch() {
     timeoutId = setTimeout("stopwatch()", 10);
 }
 
+
+// to get difference of time between last lap and now
+function getdiff(){
+  diff_hr = hr - prev_hr;
+  diff_min = min - prev_min;
+  if (diff_min < 0){
+    diff_min += 60;
+    diff_hr -= 1;
+  }
+  diff_sec = sec- prev_sec;
+  if (diff_sec < 0){
+    diff_sec += 60;
+    diff_min -= 1;
+  }
+  diff_count = count - prev_count;
+  if (diff_count < 0){
+    diff_count += 100;
+    diff_sec -= 1;
+  }
+
+  prev_count = count;
+  prev_sec = sec;
+  prev_min = min;
+  prev_hr = hr;
+}
+
+
 function lap() {
     
     if(timer){
         //displaying record container div
         $id("record-container").style.display = "block";
         
+        // calling getting difference function
+        getdiff();
+
         var lap_time = $id("hr").innerHTML + ":" 
         + $id("min").innerHTML + ":" 
         + $id("sec").innerHTML + ":" 
@@ -117,9 +157,34 @@ function lap() {
         const row = table.insertRow(0);
         const no_cell = row.insertCell(0);
         const time_cell = row.insertCell(1);
+        const diff_cell = row.insertCell(2);
         
         no_cell.innerHTML = lapCounter;
         time_cell.innerHTML = lap_time;
+
+
+
+        var hrString = diff_hr;
+        var minString = diff_min;
+        var secString = diff_sec;
+        var countString = diff_count;
+    
+        if ( diff_hr < 10) {
+            hrString = "0" + hrString;
+        }
+        if (diff_min < 10) {
+            minString = "0" + minString;
+        }
+        if (diff_sec < 10) {
+            secString = "0" + secString;
+        }
+        if (diff_count < 10) {
+            countString = "0" + countString;
+        }
+        diff_cell.innerHTML = hrString+ ":" 
+        + minString+ ":" 
+        + secString + ":" 
+        + countString;
         
         lapCounter++;
     }
